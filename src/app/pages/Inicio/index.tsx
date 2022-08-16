8c4fed9bddd24d6cc5aaf0aa1871bbae0be19f01
Felipe <felipeshimada2@gmail.com>
2022-08-16 11:53:27 -0300

    header

--- src/app/pages/Inicio/index.tsx
+++ src/app/pages/Inicio/index.tsx
@@ -1,6 +1,10 @@
+import styles from './Inicio.module.scss';
+
 export const Inicio = () => {
 
     return (
-        <p>teste inicio</p>
+        <header className={styles.header}>
+            <h1 className={styles.titulo}>Conversor de medidas</h1>
+        </header>
     )
 }
