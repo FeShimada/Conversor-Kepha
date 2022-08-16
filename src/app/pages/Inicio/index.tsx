e9738d8869c29801f54748f9f34c7139cf16f06e
Felipe <felipeshimada2@gmail.com>
2022-08-16 12:35:03 -0300

    botoes

--- src/app/pages/Inicio/index.tsx
+++ src/app/pages/Inicio/index.tsx
@@ -3,8 +3,19 @@ import styles from './Inicio.module.scss';
 export const Inicio = () => {
 
     return (
-        <header className={styles.header}>
-            <h1 className={styles.titulo}>Conversor de medidas</h1>
-        </header>
+        <section>
+            <header className={styles.header}>
+                <h1 className={styles.titulo}>Conversor de medidas</h1>
+            </header>
+
+            <div className={styles.container}>
+                <ul className={styles.container__lista}>
+                    <li><button className={styles.lista__item}>Litro</button></li>
+                    <li><button className={styles.lista__item}>Metro</button></li>
+                    <li><button className={styles.lista__item}>Quilo</button></li>
+                    <li><button className={styles.lista__item}>Graus Celcius</button></li>
+                </ul>
+            </div>
+        </section>
     )
 }
